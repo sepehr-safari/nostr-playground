d6403a0ec1480ea12a29f8868ed3f6f4d788ee65
Sepehr Safari <safari.sepehr@gmail.com>
2023-09-30 14:46:18 +0330

    init

---
+++ src/Toast.tsx
@@ -0,0 +1,19 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const Toast = () => {
+  return (
+    <ToastContainer
+      position="top-left"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
