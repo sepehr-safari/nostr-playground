b7282b2e8da95cc3457ef7edb42724290ee86053
Sepehr Safari <safari.sepehr@gmail.com>
2023-10-04 22:20:39 +0330

    add relative paths and chunks configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-chunk': ['react', 'react-dom'],
+          'nostr-tools-chunk': ['nostr-tools'],
+        },
+      },
+    },
+  },
+});
