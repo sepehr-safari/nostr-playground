cd28a25d846ae4db584a9b8a2b73d34569eb1f8b
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-11 10:22:07 +0330

    add index for components

---
+++ src/common/components/index.ts
@@ -0,0 +1,7 @@
+export * from './array-input-with-title';
+export * from './button';
+export * as Card from './card';
+export * from './input-with-title';
+export * from './readonly-input-with-title';
+export * from './remove-button';
+export * from './toast';
