0a3c97756dbcac7dcfd71c6dd857193aaf3eefe6
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-25 16:00:55 +0330

    fix input with title add rest props

--- src/common/components/input-with-title/index.tsx
+++ src/common/components/input-with-title/index.tsx
@@ -2,7 +2,7 @@ type Props = {
   title: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export const InputWithTitle = ({ title }: Props) => {
+export const InputWithTitle = ({ title, ...props }: Props) => {
   return (
     <div className="rounded-md border border-gray-300 p-4 duration-200 hover:border-indigo-600">
       <h4 className="block text-sm font-medium text-gray-700">{title}</h4>
@@ -10,6 +10,7 @@ export const InputWithTitle = ({ title }: Props) => {
       <input
         className="mt-4 block w-full rounded-md border-0 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
         autoComplete="off"
+        {...props}
       />
     </div>
   );
